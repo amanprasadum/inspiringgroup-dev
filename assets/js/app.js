e15d0acf50f4b08963aee8a0c42d215e2eaeeb35
AmanPrasad <amanprasad.um@gmail.com>
2025-10-15 15:15:18 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-
+// animation on scroll scripts
 AOS.init({
     duration: 1000,
     once: false,
@@ -26,3 +26,39 @@ window.addEventListener('scroll', function () {
         navbar.style.boxShadow = 'none';
     }
 });
+// scroll to top scripts
+const btn = document.getElementById('scrollTopBtn');
+const circle = document.querySelector('.progress-ring-circle');
+const circumference = 2 * Math.PI * 30;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = circumference;
+
+function updateProgress() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const progress = scrollTop / scrollHeight;
+    const offset = circumference - progress * circumference;
+    circle.style.strokeDashoffset = offset;
+}
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > 300) {
+        btn.classList.add('show');
+    } else {
+        btn.classList.remove('show');
+    }
+
+    updateProgress();
+});
+
+btn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+updateProgress();
